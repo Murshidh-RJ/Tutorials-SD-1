b2b4fa1c730d4509aa285c1ca70124172f27fbfc
Abdhulla Murshidh <murshidhraji2003@gmail.com>
2024-10-11 21:43:29 +0530

    task 10 done

---
+++ WEEK -1/Task-10.py
@@ -0,0 +1,20 @@
+"""Task 10: Temperature Conversion: 
+
+Create a program to convert a temperature from Celsius to Fahrenheit and vice versa.
+
+"""
+
+# Input temperature value and type of conversion
+temp = float(input("Enter the temperature value: "))
+conversion_type = input("Convert to (F)ahrenheit or (C)elsius? ").lower()
+
+# Celsius to Fahrenheit conversion
+if conversion_type == 'f':
+    fahrenheit = (temp * 9/5) + 32
+    print(f"{temp}°C is {fahrenheit}°F")
+# Fahrenheit to Celsius conversion
+elif conversion_type == 'c':
+    celsius = (temp - 32) * 5/9
+    print(f"{temp}°F is {celsius}°C")
+else:
+    print("Invalid input! Please choose 'F' or 'C'.")
