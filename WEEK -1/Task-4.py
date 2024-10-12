a26bcf961fad7e176a6b5dbac74e027f81987f65
Abdhulla Murshidh <murshidhraji2003@gmail.com>
2024-10-12 08:00:33 +0530

    test 4 done

---
+++ WEEK -1/Task-4.py
@@ -0,0 +1,11 @@
+#Task 4
+'''
+Task 4: String Concatenation
+Create two string variables: first_name = "John" and last_name = "Doe".
+Concatenate them: full_name = first_name + " " + last_name.
+Print the full name: print(full_name).
+'''
+first_name = "Jhon"
+second_name = "Doe"
+full_name = first_name+" "+second_name
+print(full_name)
