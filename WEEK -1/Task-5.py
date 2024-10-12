c564feef471437fe4fb3822bbb2c41f6ab9c099a
Abdhulla Murshidh <murshidhraji2003@gmail.com>
2024-10-12 08:02:13 +0530

    done task 5

---
+++ WEEK -1/Task-5.py
@@ -0,0 +1,10 @@
+#Task 5
+'''
+Task 5: Basic Error Handling
+Deliberately make a syntax error: print("Hello.
+Run the code to see the error message.
+Correct the error and re-run: print("Hello").
+'''
+
+print("hello
+print("Hello")
