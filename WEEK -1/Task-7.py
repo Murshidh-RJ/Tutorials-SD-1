4c4776643ff19a647614a76010eadd80c2ebf44b
Abdhulla Murshidh <murshidhraji2003@gmail.com>
2024-10-12 08:15:23 +0530

    task 7 done

---
+++ WEEK -1/Task-7.py
@@ -0,0 +1,9 @@
+'''
+Task 7: Personal Information Script: 
+Write a program that asks for the user's name, age, and favorite color, and then prints a personalized greeting.
+
+'''
+name = input("please enter your name :")  #asking user name as a input
+age = input("please enter your age : ")  #asking age as input
+fav_clr = input("what is your Favorite color : ")  #asking favorite color
+print("Hello ",name,". You are ",age," Years old. Your favorite color is ",fav_clr,".")
